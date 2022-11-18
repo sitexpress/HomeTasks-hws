6f696728bef937ac1e401373d9b77f9bf1cc028c
Sultan <sitexpressorg@gmail.com>
2022-11-18 20:56:39 +0400

    css refactoring

--- src/s1-main/App.tsx
+++ src/s1-main/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import s from './App.module.css'
 import HW1 from '../s2-homeworks/hw01/HW1'
+import HW2 from "../s2-homeworks/hw02/HW2";
 
 function App() {
     return (
