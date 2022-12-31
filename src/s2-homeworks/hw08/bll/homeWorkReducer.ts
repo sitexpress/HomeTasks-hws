2fc66afbbded6133f308c961ea23b0c3071504c0
Sultan <sitexpressorg@gmail.com>
2022-12-31 13:50:55 +0400

    HW8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,19 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
 
-            return state // need to fix
+            // return state // need to fix
+            return action.payload === 'up'
+                ? [...state].sort((a,b) => a.name > b.name ? 1 : -1)
+                : [...state].sort((a,b) => b.name > a.name ? 1 : -1)
         }
         case 'check': {
 
-            return state // need to fix
+            // return state // need to fix
+            return state.filter(el => el.age > 18)
         }
         default:
             return state
